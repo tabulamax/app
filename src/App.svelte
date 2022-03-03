f4fe0c95a50297484394d1063b49b254601d2af2
Diogenes <diogenesofweb@gmail.com>
2022-03-03 10:34:01 +0200

    add stop-war

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script>
   import Index from './view/Index.svelte';
   import AppFooter from '$lib/AppFooter.svelte';
+  import StopWar from './lib/StopWar.svelte';
 </script>
+
+<StopWar />
 
 <div>
   <Index />
---
+++ src/lib/StopWar.svelte
@@ -0,0 +1,19 @@
+<section id="stop-war">
+  <a href="https://ukraine.ua/news/stand-with-ukraine/" target="_blank" rel="noopener noreferrer">
+    <span>STOP WAR. STAND WITH UKRAINE 🇺🇦</span>
+  </a>
+</section>
+
+<style>
+  #stop-war {
+    padding: 1rem;
+    background-color: black;
+    font-size: 16px;
+    text-align: center;
+  }
+
+  #stop-war a {
+    color: red;
+    text-decoration: none;
+  }
+</style>
