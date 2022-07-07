0dceb4ddd997d522b11e5a354bb4c3bb851195e2
Diogenes <diogenesofweb@gmail.com>
2022-07-07 12:44:23 +0300

    up

--- src/lib/Diagram.svelte
+++ src/lib/Diagram.svelte
@@ -2,7 +2,6 @@
   import { Btn, Modal } from '@kazkadien/svelte';
   import { generateCode } from '../utils/generateCode';
   import ModalCard from './ModalCard.svelte';
-  // import MyIcon from './MyIcon.svelte';
 
   let html = '';
   let isModal = false;
@@ -11,15 +10,14 @@
   let mermaid;
 
   async function onClick() {
-    // console.log('open');
     isModal = true;
 
     if (!mermaid) {
       mermaid = (await import('mermaid')).default;
     }
 
+    // @ts-ignore
     mermaid.initialize({ securityLevel: 'loose', theme: 'default' });
-    // mermaid.initialize({ securityLevel: 'loose', theme: 'dark' });
 
     const input = generateCode('erd');
 
@@ -27,7 +25,6 @@
       if (svgCode.length > 0) {
         // console.log(svgCode);
         html = svgCode;
-        // container.innerHTML = svgCode;
       }
     });
   }
@@ -40,13 +37,6 @@
 {#if isModal}
   <Modal on:close={() => (isModal = false)}>
     <ModalCard fullWidth>
-      <!-- <header class="fdc"> -->
-      <!--   <Btn on:click variant="outlined" round> -->
-      <!--     <MyIcon name="content_copy" /> -->
-      <!--     <b>Copy To Clipboard</b> -->
-      <!--   </Btn> -->
-      <!-- </header> -->
-
       <div id="erd">
         {@html html}
       </div>
@@ -57,7 +47,6 @@
 <style global>
   #erd {
     background-color: white;
-    /* background-color: black; */
     border-radius: 1rem;
   }
 </style>
